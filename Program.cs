ec378807b80cc5cd2dc36411da531a269e202b48
Albert <s-albert@mail.ru>
2020-02-18 22:13:04 +0500

    Урок №7

--- Program.cs
+++ Program.cs
@@ -16,33 +16,14 @@ namespace Snake
             Point p3 = new Point(3, 3, '$');
             Point p4 = new Point(4, 4, '&');
 
+            HorizontalLine hLine = new HorizontalLine(1, 15, 8, '=');
+            hLine.DrawAll();
 
-            List<Point> pList = new List<Point>();      // объявляем лист с точками и заполняем:
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+            VerticalLine vLine = new VerticalLine(1, 15, 8, '|');
+            vLine.DrawAll();
 
-            //выведем все точки в консоль:
-            foreach (var p in pList)
-            {
-                p.Draw();
-            }
 
-            //добавим отступ:
-            Console.WriteLine();
 
-            List<char> cList = new List<char>();    // объявляем лист с символами и заполняем:
-            cList.Add('*');
-            cList.Add('|');
-            cList.Add('+');
-            cList.Add('=');
-
-            //выведем все символы в консоль:
-            for (var c = 0; c < cList.Count; c++)
-            {
-                Console.Write(cList[c] + " ");
-            }
 
             Console.ReadKey();
         }
